6309e92be3a721699dbad2411f55dedcb544b79f
loganfidler <loganfidler@gmail.com>
2020-06-12 22:44:04 -0600

    Append li with proper names to nav menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 // Get and store the data-nav attribute of each section
 let sections = document.querySelector('.page__main').children;
 
-var navArray = [];
+var navNames = [];
 
 for (let i = 0; i < sections.length; i++) {
     let navName = sections[i].dataset.nav;
-    navArray.push(navName);
+    navNames.push(navName);
 }
+
+// Select the nav list, create li elements with proper section names and append to nav list
+const navMenu = document.querySelector('.nav__list');
+const fragment = document.createDocumentFragment();
+const navListClassName = "nav__item";
+
+for (const navName of navNames) {
+    const listItem = document.createElement('li');
+    listItem.classList.add(navListClassName);
+    listItem.innerHTML = navName;
+    fragment.appendChild(listItem);
+}
+
+navMenu.appendChild(fragment);
