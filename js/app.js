1b4391fe05ea4a0877a9cdf83ba4b9454b5a5034
loganfidler <loganfidler@gmail.com>
2020-06-14 21:19:26 -0600

    Basic Intersection Observer setup for each section

--- js/app.js
+++ js/app.js
@@ -30,3 +30,27 @@ for (let i = 0; i < sections.length; i++) {
 
 navMenu.appendChild(fragment);
 
+// INTERSECTION OBSERVER
+
+const sectionOne = document.querySelector('#container-one');
+const allSections = document.querySelectorAll(".section__container");
+
+const options = {
+    root: null, // the viewport
+    threshold: 1, // a 0 - 1 scale; at 1, it will fire when 100% of the target is showing on the screen
+    rootMargin: "0px"
+ };
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+           return;
+        }
+        console.log(entry);
+        entry.target.classList.toggle('focus');
+    })
+}, options);
+
+allSections.forEach(section => {
+    observer.observe(section);
+})
